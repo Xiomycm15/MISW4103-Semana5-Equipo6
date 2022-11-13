5079802740ab030b839530f73c99ffc3c3d6e335
Lara <the.lemonetti@gmail.com>
2022-11-13 02:47:40 -0800

    Se añade la primera feature de Kraken

---
+++ kraken/features/feature1_iniciosesion.feature
@@ -0,0 +1,13 @@
+Feature: Inicio de sesión
+
+@user1 @web
+Scenario: Inicio sesión y entro a mi sitio
+  Given I navigate to page "http://localhost:2368/ghost/#/signin"
+  And I wait for 5 seconds
+  When I enter email "<USERNAME1>"
+  And I wait for 2 seconds
+  And I enter password "<PASSWORD1>"
+  And I wait for 2 seconds
+  And I click sign in
+  And I wait for 7 seconds
+  And I click on view site
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,1 +1,21 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+When('I enter email {kraken-string}', async function (email) {
+    let element = await this.driver.$('#ember6');
+    return await element.setValue(email);
+});
+
+When('I enter password {kraken-string}', async function (password) {
+    let element = await this.driver.$('#ember8');
+    return await element.setValue(password);
+});
+
+When('I click sign in', async function() {
+    let element = await this.driver.$('#ember10');
+    return await element.click();
+})
+
+When('I click on view site', async function() {
+    let element = await this.driver.$('#ember23');
+    return await element.click();
+})
